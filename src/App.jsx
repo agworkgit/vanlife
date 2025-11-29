5c908de701ebf867a4d30b0e539f36257963bc3c
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-11-29 22:25:16 +0000

    fix: improved routing with nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Vans from "./pages/vans/Vans";
 import VanDetail from "./pages/vans/VanDetail";
 // Host
 import HostLayout from "./pages/host/HostLayout";
+import Dashboard from "./pages/host/Dasboard";
 import Income from "./pages/host/Income";
 import Reviews from "./pages/host/Reviews";
 
@@ -21,18 +22,24 @@ function App() {
    return (
       <BrowserRouter>
          <Routes>
-            <Route element={<Layout />}>
+            <Route path="/" element={<Layout />}>
                {/* Child routes*/}
-               <Route path="/" element={<Home />} />
-               <Route path="/about" element={<About />} />
-               <Route path="/vans" element={<Vans />} />
-               {/* :id represents a variable inside our path (see useParams() to access the variable),
-                  we can also add multiple variables if we want i.e. :id/:type etc.. */}
-               <Route path="/vans/:id" element={<VanDetail />} />
 
-               <Route path="/host" element={<HostLayout />}>
-                  <Route path="/host/income" element={<Income />} />
-                  <Route path="/host/reviews" element={<Reviews />} />
+               {/* Home, About pages routes*/}
+               <Route index element={<Home />} />
+               <Route path="about" element={<About />} />
+
+               {/* Vans routes, a nested setup is not required here due to no shared UI */}
+               <Route path="vans" element={<Vans />} />
+               {/* :id represents a variable inside our path (see useParams() to access the variable), we can also add multiple variables if we want i.e. :id/:type etc.. */}
+               <Route path="vans/:id" element={<VanDetail />} />
+
+               {/* Host routes*/}
+               <Route path="host" element={<HostLayout />}>
+                  {/* 'index' is a workaround in Router and it replaces "/" absolute path */}
+                  <Route index element={<Dashboard />} />
+                  <Route path="income" element={<Income />} />
+                  <Route path="reviews" element={<Reviews />} />
                </Route>
             </Route>
          </Routes>
